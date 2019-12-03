1da9ead836a9614ac3d36c9e0ef9f853ac25d02a
xFlowDev <xflowdev@protonmail.com>
2019-12-03 21:12:30 +0100

    Added rudimentary gui elements

--- src/fhswf/floed/Controller.java
+++ src/fhswf/floed/Controller.java
@@ -1,4 +1,26 @@
 package fhswf.floed;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class Controller {
+    @FXML
+    public MenuItem testMenuItem;
+
+    public Controller() {
+    }
+
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
+    private void test() {
+        // This works already
+        System.out.println("Hallo Welt");
+        // Set up the rest of the UI tomorrow
+        // Use a GridPane to display the table
+        // Use a
+    }
 }
--- src/fhswf/floed/Main.java
+++ src/fhswf/floed/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainOverview.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Notenverwaltung");
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
 
